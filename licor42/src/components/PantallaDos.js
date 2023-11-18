271316bc751545d1f6b1f6ddfcb6307933d88f14
AdrianSevert <adriansevertsilva@gmail.com>
2023-11-18 01:31:24 +0100

    hola

--- licor42/src/components/PantallaDos.js
+++ licor42/src/components/PantallaDos.js
@@ -4,43 +4,48 @@ function PantallaDos() {
 	return (
 		<div className="pantalla-dos">
 			<div className="container-left">
-				Container-left
 				<div className="container-up-left">
-					Container-up-left
-					<div className="Motores-1-4">
-						Motores-1-4
-						<div className="grafana-motor">mg1</div>
-						<div className="grafana-motor">mg2</div>
-						<div className="grafana-motor">mg3</div>
-						<div className="grafana-motor">mg4</div>
+					<div className="motores-1-4">
+						<div className="grafana-motor-1"><iframe src="http://192.168.0.142:3000/d-solo/c5dfcc6b-5114-42e9-b6ad-7986f614dcad/new-dashboard?orgId=1&from=1700240125653&to=1700261725653&panelId=1" height="200px" width="120px"></iframe></div>
+						<div className="grafana-motor-1"><iframe src="http://192.168.0.142:3000/d-solo/c5dfcc6b-5114-42e9-b6ad-7986f614dcad/new-dashboard?orgId=1&from=1700240125653&to=1700261725653&panelId=1" height="200px" width="120px"></iframe></div>
+						<div className="grafana-motor-1"><iframe src="http://192.168.0.142:3000/d-solo/c5dfcc6b-5114-42e9-b6ad-7986f614dcad/new-dashboard?orgId=1&from=1700240125653&to=1700261725653&panelId=1" height="200px" width="120px"></iframe></div>
+						<div className="grafana-motor-1"><iframe src="http://192.168.0.142:3000/d-solo/c5dfcc6b-5114-42e9-b6ad-7986f614dcad/new-dashboard?orgId=1&from=1700240125653&to=1700261725653&panelId=1" height="200px" width="120px"></iframe></div>
 					</div>
-					<div className="Motores-5-7">
-						<div className="grafana-motor">mg5</div>
-						<div className="grafana-motor">mg6</div>
-						<div className="grafana-motor">mg7</div>
+					<div className="motores-5-7">
+						<div className="grafana-motor-2"><iframe src="http://192.168.0.142:3000/d-solo/c5dfcc6b-5114-42e9-b6ad-7986f614dcad/new-dashboard?orgId=1&from=1700240125653&to=1700261725653&panelId=1" height="200px" width="120px"></iframe></div>
+						<div className="grafana-motor-2"><iframe src="http://192.168.0.142:3000/d-solo/c5dfcc6b-5114-42e9-b6ad-7986f614dcad/new-dashboard?orgId=1&from=1700240125653&to=1700261725653&panelId=1" height="200px" width="120px"></iframe></div>
+						<div className="grafana-motor-2"><iframe src="http://192.168.0.142:3000/d-solo/c5dfcc6b-5114-42e9-b6ad-7986f614dcad/new-dashboard?orgId=1&from=1700240125653&to=1700261725653&panelId=1" height="200px" width="120px"></iframe></div>
 					</div>
 				</div>
 				<div className="container-down-left">
 					<div className="grafana-horizonte">
-						Bastardada de horizonte artificial
+					<iframe src="http://192.168.0.142:3000/d-solo/c5dfcc6b-5114-42e9-b6ad-7986f614dcad/new-dashboard?orgId=1&from=1700240125653&to=1700261725653&panelId=1" height="100%" width="90%"></iframe>
 					</div>
 				</div>
 			</div>
 			<div className="container-right">
-				Container-right
 				<div className="container-up-right">
-					Container-up-right
 					<div className="kpi-1-2">
-						<div className="grafana-kpi">kpi1</div>
-						<div className="grafana-kpi">kpi2</div>
+						<div className="grafana-kpi">
+						<iframe src="http://192.168.0.142:3000/d-solo/c5dfcc6b-5114-42e9-b6ad-7986f614dcad/new-dashboard?orgId=1&from=1700240125653&to=1700261725653&panelId=1" height="100%" width="100%"></iframe>
+						</div>
+						<div className="grafana-kpi">
+						<iframe src="http://192.168.0.142:3000/d-solo/c5dfcc6b-5114-42e9-b6ad-7986f614dcad/new-dashboard?orgId=1&from=1700240125653&to=1700261725653&panelId=1" height="100%" width="100%"></iframe>
+						</div>
 					</div>
 					<div className="kpi-3-4">
-						<div className="grafana-kpi">kpi3</div>
-						<div className="grafana-kpi">kpi4</div>
+						<div className="grafana-kpi">
+						<iframe src="http://192.168.0.142:3000/d-solo/c5dfcc6b-5114-42e9-b6ad-7986f614dcad/new-dashboard?orgId=1&from=1700240125653&to=1700261725653&panelId=1" height="100%" width="100%"></iframe>
+						</div>
+						<div className="grafana-kpi">
+						<iframe src="http://192.168.0.142:3000/d-solo/c5dfcc6b-5114-42e9-b6ad-7986f614dcad/new-dashboard?orgId=1&from=1700240125653&to=1700261725653&panelId=1" height="100%" width="100%"></iframe>
+						</div>
 					</div>
 				</div>
 				<div className="container-down-right">
-					<div className="grafana-alerts">IMS</div>
+					<div className="grafana-alerts">
+					<iframe src="http://192.168.0.142:3000/d-solo/c5dfcc6b-5114-42e9-b6ad-7986f614dcad/new-dashboard?orgId=1&from=1700240125653&to=1700261725653&panelId=1" height="100%" width="94%"></iframe>
+					</div>
 				</div>
 			</div>
 		</div>
--- licor42/src/components/PantallaUno.js
+++ licor42/src/components/PantallaUno.js
@@ -1,12 +1,88 @@
-import React from "react";
+import React, { useMemo }  from "react";
+
+// Función para calcular el ancho del cable
+const calculateCableWidth = (kw) => {
+	const maxKw = 3384;
+	const maxWidth = 14; // Máximo ancho en píxeles
+	const minWidth = 1; // Mínimo ancho en píxeles
+	return Math.max((kw / maxKw) * maxWidth, minWidth);
+  };
+
+  // Componente que representa el cable de flujo de cada motor
+const FlowCable = ({ kw }) => (
+	<div className="flow-cable" style={{ width: `${calculateCableWidth(kw)}px` }}></div>
+  );
+
+  // Componente que representa cada motor junto con su cable
+const MotorWithCable = ({ name, kw }) => (
+	<div className="motor-with-cable">
+	  <div className="motor-box">
+		<div className="motor-label">{name}</div>
+	  </div>
+	  <FlowCable kw={kw} />
+	</div>
+  );
+
+// Componente para el consumo intermedio
+const IntermediateConsumption = ({ className }) => (
+	<div className={className}>
+	  <div className={`${className}-box`}>Consumo Intermedio</div>
+	  <div className={`${className}-cable`}></div>
+	</div>
+  );
+  
+  // Componente principal
+  const App = () => {
+	// Crear un array de motores con valores simulados de kW
+	const motors = Array.from({ length: 7 }, (_, i) => ({ name: `M${i + 1}`, kw: Math.random() * 3384 }));
+  
+	// Calcular la suma total de los anchos de los cables
+	const totalCableWidth = useMemo(() => {
+	  return motors.reduce((sum, motor) => sum + calculateCableWidth(motor.kw), 0);
+	}, [motors]);
+  
+	// Calcular el desplazamiento necesario para mantener el conector vertical centrado
+	const verticalConnectorOffset = useMemo(() => {
+	  return totalCableWidth / 2;
+	}, [totalCableWidth]);
+};
 
 function PantallaUno() {
+	// Crear un array de motores con valores simulados de kW
+	const motors = Array.from({ length: 7 }, (_, i) => ({ name: `M${i + 1}`, kw: Math.random() * 3384 }));
+  
+	// Calcular la suma total de los anchos de los cables
+	const totalCableWidth = useMemo(() => {
+	  return motors.reduce((sum, motor) => sum + calculateCableWidth(motor.kw), 0);
+	}, [motors]);
+  
+	// Calcular el desplazamiento necesario para mantener el conector vertical centrado
+	const verticalConnectorOffset = useMemo(() => {
+	  return totalCableWidth / 2;
+	}, [totalCableWidth]);
+
 	return (
-		<div className="pantalla-uno">
-			<h1>Bienvenido a la Pantalla Uno</h1>
-			<p>Este es un ejemplo de contenido para la primera pantalla.</p>
+		<div className="diagram-container">
+		  <div className="motors-container">
+			{motors.map(motor => (
+			  <MotorWithCable key={motor.name} name={motor.name} kw={motor.kw} />
+			))}
+		  </div>
+		  <div className="horizontal-connector"></div>
+		  <div 
+			className="vertical-connector" 
+			style={{ 
+			  width: `${totalCableWidth}px`, 
+			  left: `calc(50% - ${verticalConnectorOffset}px)` 
+			}}
+		  ></div>
+		  <IntermediateConsumption className="intermediate-consumption" />
+		  <IntermediateConsumption className="left-intermediate-consumption" />
+		  <div className="plant-box">
+			<div className="plant">Planta de Producción</div>
+		  </div>
 		</div>
-	);
+	  );
 }
 
 export default PantallaUno;
